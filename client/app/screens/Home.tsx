5076782462e3f2befe4a337fd52c8a62ca0e9be4
Mateus S. Neubarth <mateusneubarth@hotmail.com>
2024-04-22 16:50:20 -0300

    Finished client-side logic and styles

--- client/app/screens/Home.tsx
+++ client/app/screens/Home.tsx
@@ -1,7 +1,8 @@
-import { Text, View } from "react-native";
+import { Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as SecureStore from 'expo-secure-store';
 import axios from "axios";
+import Task from "../components/Task";
 
 interface Todo {
     _id: string;
@@ -13,30 +14,135 @@ interface Todo {
 };
 
 const Home = () => {
+    const [todo, setTodo] = useState<string>();
     const [todoList, setTodoList] = useState<Todo[]>([]);
     
+    const setTodos = async () => {
+        try{
+            const USER_ID = await SecureStore.getItemAsync('USER_ID');
+            const url = 'http://10.0.2.2:8080/api/todo/' + USER_ID?.replaceAll('"', '');
+            const response = await axios.get(url);
+            setTodoList(response.data);
+        } catch(err) {
+            console.log("Error found ~ " + err);
+        }
+    };
 
     useEffect(() => {
-        const setTodos = async () => {
-            try{
-                const USER_ID = await SecureStore.getItemAsync('USER_ID');
-                const url = 'http://10.0.2.2:8080/api/todo/' + USER_ID?.replaceAll('"', '')
-                const response = await axios.get(url);
-                setTodoList(response.data);
-            } catch(err) {
-                console.log("Error found ~ " + err);
-            }
-        };
         setTodos();
-    },[]);
+    }, []);
+
+    const handleAddTask = async () => {
+        Keyboard.dismiss();
+        try {
+            const USER_ID = await SecureStore.getItemAsync('USER_ID');
+            const url = 'http://10.0.2.2:8080/api/todo/' + USER_ID?.replaceAll('"', '');
+            const response = await axios.post(url, { todoText: todo });
+            setTodoList([...todoList, response.data])
+        } catch (err) {
+            console.log("Error found ~ " + err);
+        }
+    };
+
+    const completeTask = async (task_id: string) => {
+        try {
+            const url = 'http://10.0.2.2:8080/api/todo/' + task_id;
+            await axios.delete(url);
+            setTodos();
+        } catch (err) {
+            console.log("Error found ~ "+ err);
+        }
+    };
 
     return (
-        <View>
-            {todoList.map((todo: Todo, key) => {
-                return <Text key={key}>{todo.todoText}</Text>
-            })}
+        <View style={styles.container}>
+
+      {/* Today's Tasks */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Your tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          {
+            todoList.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() =>completeTask(item._id)}>
+                  <Task text={item.todoText} />
+                </TouchableOpacity>
+              )
+            })
+          }
         </View>
+      </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        //behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput 
+          style={styles.input} 
+          placeholder={'Write a task'}
+          value={todo}
+          onChangeText={text => setTodo(text)} 
+        />
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+    </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#E8EAED',
+    },
+    taskWrapper: {
+      paddingTop: 80,
+      paddingHorizontal: 20,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: 'bold'
+    },
+    items: {
+      marginTop: 30,
+    },
+    writeTaskWrapper: {
+      position: 'absolute',
+      bottom: 60,
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    input: {
+      paddingVertical: 15,
+      paddingHorizontal: 15,
+      backgroundColor: '#FFF',
+      borderRadius: 60,
+      borderColor: '#C0C0C0',
+      borderWidth: 1,
+      width: 250,
+    },
+    addWrapper: {
+      width: 60,
+      height: 60,
+      backgroundColor: '#FFF',
+      borderRadius: 60,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderColor: '#C0C0C0',
+      borderWidth: 1,
+    },
+    addText: {}
+  });
+  
 
 export default Home;
--- client/app/screens/Login.tsx
+++ client/app/screens/Login.tsx
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 
@@ -6,6 +6,7 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
+    const [isLogin, setIsLogin] = useState(true);
     const { onLogin, onRegister } = useAuth();
 
     const login = async () => {
@@ -26,24 +27,65 @@ const Login = () => {
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.form}>
-                <TextInput 
-                    style={styles.input} 
-                    placeholder="Email" 
-                    onChangeText={(text: string) => setEmail(text)} 
-                    value={email}
-                />
-                <TextInput 
-                    style={styles.input} 
-                    placeholder="Password" 
-                    secureTextEntry={true} 
-                    onChangeText={(text: string) => setPassword(text)}
-                    value={password}
-                />
-                <Button onPress={login} title="Login" />
-            </View>
-        </View>
+        <>
+            {isLogin ? (
+                <View style={styles.container}>
+                    <View style={styles.form}>
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Email" 
+                            onChangeText={(text: string) => setEmail(text)} 
+                            value={email}
+                        />
+                        <TextInput 
+                            style={styles.input} 
+                            placeholder="Password" 
+                            secureTextEntry={true} 
+                            onChangeText={(text: string) => setPassword(text)}
+                            value={password}
+                        />
+                        <Button onPress={login} title="Login" />
+                    </View>
+                    <Pressable 
+                            onPress={() => setIsLogin(false)}
+                            style={styles.pressable}    
+                        >
+                            <Text>Not registered? Register now!</Text>
+                    </Pressable>
+                </View>
+            ) : (
+                <View style={styles.container}>
+                    <View style={styles.form}>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Username"
+                            onChangeText={(text: string) => setUsername(text)}
+                            value={username}
+                        />
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Email"
+                            onChangeText={(text: string) => setEmail(text)}
+                            value={email}
+                        />
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Password"
+                            secureTextEntry={true} 
+                            onChangeText={(text: string) => setPassword(text)}
+                            value={password}
+                        />
+                        <Button onPress={register} title="Register" />
+                    </View>
+                    <Pressable 
+                            onPress={() => setIsLogin(true)}
+                            style={styles.pressable}    
+                        >
+                            <Text>Already have an account? Login here!</Text>
+                    </Pressable>
+                </View>
+            )}
+        </>
     );
 };
 
@@ -62,6 +104,9 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         width: '100%'
+    },
+    pressable: {
+
     }
 });
 
